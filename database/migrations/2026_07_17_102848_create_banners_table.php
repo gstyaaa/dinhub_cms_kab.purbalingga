401d436c851d4d126e51639294db162f8aee8cb6
Agastya Itsar Maulana <agaspurballingga@gmail.com>
2026-07-31 10:06:04 +0700

    fix(database): make title column nullable in banners table

--- database/migrations/2026_07_17_102848_create_banners_table.php
+++ database/migrations/2026_07_17_102848_create_banners_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
         Schema::create('banners', function (Blueprint $table) {
             $table->id();
 
-            $table->string('title');
+            $table->string('title')->nullable();
             $table->string('subtitle')->nullable();
 
             $table->string('image');
